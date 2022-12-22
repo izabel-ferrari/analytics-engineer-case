069775bc8fca20ee16fd1d52cbc54d810013509a
Izabel Ferrari <ferrari.izabel@gmail.com>
2022-12-22 16:42:53 -0300

    Add source tests

--- part1_companies_data_elt/data_transformation/models/stg/companies_stg.sql
+++ part1_companies_data_elt/data_transformation/models/stg/companies_stg.sql
@@ -1,8 +1,15 @@
 SELECT
     id AS company_id,
     name AS company_name,
-    CASE
-        WHEN UPPER(country) = 'BRA' THEN 'BR'
+    CASE UPPER(country)
+        WHEN 'BRAZIL' THEN 'BR'
+        WHEN 'BRASIL' THEN 'BR'
+        WHEN 'BRA' THEN 'BR'
+        WHEN 'MEXICO' THEN 'MX'
+        WHEN 'MÉXICO' THEN 'MX'
+        WHEN 'MEX' THEN 'MX'
+        WHEN 'COLOMBIA' THEN 'CO'
+        WHEN 'COL' THEN 'CO'
         ELSE UPPER(country)
     END AS company_country
 FROM {{ source('raw_data', 'companies') }}
