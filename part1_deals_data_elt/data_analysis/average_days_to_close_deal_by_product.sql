701685e79332f13162c722655693f5b1ac3cc20d
izabel-ferrari <ferrari.izabel@gmail.com>
2022-12-30 17:40:48 -0300

    Update average_days_to_close_deal_by_product.sql

--- part1_deals_data_elt/data_analysis/average_days_to_close_deal_by_product.sql
+++ part1_deals_data_elt/data_analysis/average_days_to_close_deal_by_product.sql
@@ -1,16 +1,10 @@
 WITH deals AS (
   SELECT * FROM `analytics-engineer-case-372415.transformed_data_final.deals_fact`
-), deal_created_closed AS (
-  SELECT
-    deal_product,
-    deal_created_date,
-    deal_closed_date,
-    DATE_DIFF(deal_closed_date, deal_created_date, DAY) AS days_until_deal_closed
-  FROM deals
-  WHERE LOWER(deal_status) = 'closed'
 )
+
 SELECT
   deal_product,
-  CAST(AVG(days_until_deal_closed) AS INT) AS avg_days_until_deal_closed
-FROM deal_created_closed
+  CAST(AVG(DATE_DIFF(deal_closed_date, deal_created_date, DAY)) AS INT) AS avg_days_to_close_deal
+FROM deals
+WHERE LOWER(deal_status) = 'closed'
 GROUP BY deal_product
